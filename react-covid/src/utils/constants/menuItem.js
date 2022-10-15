96e9e60dde8de61280ed387246bc11c2b712dce3
AsepSahrudin18 <asepsahrudin0399@gmail.com>
2022-10-15 20:40:01 +0700

    refactor navbar menu

---
+++ react-covid/src/utils/constants/menuItem.js
@@ -0,0 +1,24 @@
+const menuItem = [
+  {
+    id: 1,
+    item: "Global",
+    path: "/",
+  },
+  {
+    id: 2,
+    item: "Indonesia",
+    path: "/covid/indonesia",
+  },
+  {
+    id: 3,
+    item: "Provinsi",
+    path: "/covid/provinsi",
+  },
+  {
+    id: 4,
+    item: "About",
+    path: "/covid/about",
+  },
+];
+
+export default menuItem;
