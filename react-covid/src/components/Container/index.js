998f4400946ba26b7410a6fc1ba62cd687b32f2c
AsepSahrudin18 <asepsahrudin0399@gmail.com>
2022-10-15 20:52:38 +0700

    refactor container

--- react-covid/src/components/Container/index.js
+++ react-covid/src/components/Container/index.js
@@ -1,7 +1,7 @@
 import StyledContainer from "./Container.sytled";
 
-function Container(props) {
-  return <StyledContainer>{props.children}</StyledContainer>;
+function Container({ children }) {
+  return <StyledContainer>{children}</StyledContainer>;
 }
 
 export default Container;
